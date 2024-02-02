a88d34eacb46e87c6fd4ecc134c5233a5b770e3f
aksurcos <>
2024-02-02 17:58:46 +0000

    creating warzone

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class warZone:
+    def __init__(self, n_row, n_column):        
+        self.n_row = row
+        self.n_column = n_column
+        self.war_zone = self.init_war_zone()
+        self.other_war_zone = self.init_war_zone()
+    
+    def init_war_zone(self):
+        matrix = []
+        for row in range(self.n_row):
+            matrix.append(["-"]*self.n_column)
+        return matrix
+    
