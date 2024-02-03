e21b13634258709e749ddb5ad8de37b345d0da2e
aksurcos <>
2024-02-03 15:57:23 +0000

    placing ships process

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ class warZone:
     def __init__(self, n_row, n_column):        
         self.n_row = n_row
         self.n_column = n_column
+        self.ship_shape = "O"
         self.war_zone = self.init_war_zone()
         self.other_war_zone = self.init_war_zone()
     
@@ -12,6 +13,13 @@ class warZone:
         for row in range(self.n_row):
             matrix.append(["o"]*self.n_column)
         return matrix
+
+    def place_ships(self, ship, row_number, column_number, horizontal_vertical):
+        for i in range(ship): gemi = 4
+            if vertical == "vertical:"
+                self.war_zone[row_number][column_number] = self.ship_shape
+            else:
+                self.war_zone[row_number+i][column_number = self.ship_shape]
 
     def __str__(self):
         our_war_zone = self.create_war_zone(self.war_zone)
@@ -55,5 +63,50 @@ class warZone:
             war_zone_str += row_number + space_str + row_str + "\n"
         return war_zone_str
 
-create_war_zone = warZone(15, 15)
-print(create_war_zone)
+war_zone = warZone(15, 15)
+print(war_zone)
+
+class warShips:
+    def __init__(self, ships, war_zone):
+        self.ships= ships # [3, 5, 6, 3, 4]
+        self.war_zone = war_zone
+
+    def enter_ships_coordinates(self):
+        for ship in self.ships:
+            row_number = None
+            column_number = None
+            horizontal_vertical = None
+            while row_number is None or column_number is None or horizontal_vertical is None:
+                try:
+                    given_values = self.give_values(ship)
+                    row_number, column_number, horizontal_vertical = given_values.split(" ")
+                    row_number = int(row_number)
+                    column_number = int(column_number)
+                except:
+                    print("You've entered wrong values. Please, read information again.")
+                    row_number = None
+                    # column_number = None
+                    # horizontal_vertical = None
+                else:
+                    self.war_zone.place_ships(ship, row_number, column_number, horizontal_vertical)
+        print(self.war_zone)
+    
+    def give_values(self, ship):
+        given_values = input(f"""
+Please enter your {ship}  size ship's coordinates with blank between them.
+Given values by order: row_number (int), column_number (int), horizontal or vertical (str)
+Your current warzone situation:
+{self.war_zone}
+""")
+        return given_values
+
+ships = [3, 4]
+
+war_ships = warShips(ships, war_zone)
+war_ships.enter_ships_coordinates()
+
+
+
+
+
+
