820aa4beae7650be1e034bb1372fb9b089841ec5
Aysenur K. S. Coskuner <aysenurkubrasurucu@gmail.com>
2024-10-02 00:15:08 +0000

    changing board size

--- run.py
+++ run.py
@@ -1,88 +1,82 @@
 import random
+from colorama import Fore, Back, Style, init
+
+#Colorama
+init(autoreset=True)
 
 #Creating boards side by side
 def board():
-    return[['o' for _ in range(9)] for _ in range(9)]
+    return[['o' for _ in range(5)] for _ in range(5)]
 
 def boards(board1, board2):
     print("      Your Board          Its Board")
-    print("  A B C D E F G H I       A B C D E F G H I")
-    for i in range (9):
-        row1 = ' '.join(board1[i])
-        row2 = ' '.join(['X' if cell == 'X' else 'o' for cell in board2[i]])
-        print(f"{i+1} {row1}     {i+1} {row2}")
+    print("  A B C D E        A B C D E")
+    for i in range(5):
+        row1 = ' '.join([Fore.YELLOW + 'S' + Style.RESET_ALL if cell == 'S' else 
+                         Fore.GREEN + '+' + Style.RESET_ALL if cell == 'X' else 
+                         Fore.RED + 'M' + Style.RESET_ALL if cell == 'M' else 'o' 
+                         for cell in board1[i]])
+        row2 = ' '.join([Fore.GREEN + '+' + Style.RESET_ALL if cell == 'X' else 
+                         Fore.RED + 'M' + Style.RESET_ALL if cell == 'M' else 'o' 
+                         for cell in board2[i]])
+        print(f"{i+1} {row1}    {i+1} {row2}")
 
 #Place Ships Automatic or Manually
 
-def manual_ship(warzone, ships):
-    while True:
-        begin = input(f"{ships}. Please give a coordinate to place your 5 unit long ship. Example: A3").upper()
-        direction = input("Plese, enter your ship's direction. Type H for horizontal, V for vertical.").upper()
+def manual_ship(warzone, ship_number, coord):
+    col = ord(coord[0].upper()) - ord('A')
+    row = int(coord[1]) - 1
 
-        if len(begin) != 2 or begin[0] not in 'ABCDEFGHI' or begin[1] not in '123456789':
-            print("Please, type an appropriate coordinate.")
-            continue
-
-        row = int(begin[1]) - 1
-        col = ord(begin[0]) - ord('A')
-
-        if direction == 'H':
-            if col + 5 > 9:
-                print("The ship is out of the board. Try again.")
-                continue
-            if any(warzone[row][col+i] == 'S' for i in range(5)):
-                print("There is already a ship at this coordinate.")
-                continue
-            for i in range(5):
-                warzone[row][col+i] = "S"
-        elif direction == 'V':
-            if row + 5 > 9:
-                print("The ship is out of the board. Try again.")
-                continue
-            if any(warzone[row+i][col] == 'S' for i in range(5)):
-                print("There is already a ship at this coordinate.")
-                continue
-            for i in range(5):
-                warzone[row+i][col] = "S"
-        else:
-            print("Please, type an accurate direction.")
-            continue
-        break
+    if 0 <= row < 5 and 0 <= col < 5 and warzone[row][col] == 'o':
+        warzone[row][col] = 'S'
+    else:
+        print(f"{coord} is not accurate coordinate. Please try again.")
     return warzone
 
 def automatic_ship(warzone):
     while True:
-        row = random.randint(0, 8)
-        col = random.randint(0, 8)
-        direction = random.choice(['H', 'V'])
+        row = random.randint(0, 4)
+        col = random.randint(0, 4)
 
-        if direction == 'H' and col + 5 <= 9:
-            if all(warzone[row][col+i] == 'o' for i in range(5)):
-                for i in range(5):
-                    warzone[row][col+i] = 'S'
-                break
-        elif direction == 'V' and row + 5 <= 9:
-            if all(warzone[row+i][col] == 'o' for i in range(5)):
-                for i in range(5):
-                    warzone[row+i][col] = 'S'
-                break
+        if warzone[row][col] == 'o':
+            warzone[row][col] = 'S'
+            break       
     return warzone
 
 def ship_placement(warzone, manual=True):
-    for ships in [1, 2]:
-        if manual:
-            while True:
-                placement = input(f"Welcome to the Battleship Game! How would you like to place your {ships}. ship which are 5 units long? Type 'M' for manual, 'A' for automatic." ).upper()
-                if placement == 'M':
-                    warzone = manual_ship(warzone, ships)
-                    break
-                elif placement == 'A':
+    if manual:
+        while True:
+            placement = input("Welcome to the Battleship Game! You will have 1 unit long 5 ships to place. How would you like to place your ships? Type 'M' for manual or 'A' for automatic.").upper()
+            if placement == 'A':
+                for _ in range(5): #5 Ships will be placed.
                     warzone = automatic_ship(warzone)
-                    break
-                else:
-                    print("Please make an accurate choice. Type 'M' or 'A'")
-        else:
+                break
+            elif placement == 'M':
+                ships_placed = 0
+                while ships_placed < 5:
+                    coord = input(f"Give a coordinate for {ships_placed + 1}. ship. (Example: A1, B2)")
+                    if len(coord) == 2 and coord[0].isalpha() and coord[1].isdigit():
+                        col = ord(coord[0].upper()) - ord('A')
+                        row = int(coord[1]) - 1
+                        if 0 <= row < 5 and 0 <= col < 5 and warzone[row][col] == 'o':
+                            warzone[row][col] = 'S'
+                            ships_placed += 1
+                            print(f"{ships_placed}. ship is placed at {coord}.")
+                            print("\n Your current board:")
+                            print(warzone, board())
+                        else:
+                            print(f"{coord} is already full or not accurate. Please try another coordinate.")
+                    else:
+                        print("Invalid coordinate. Please check and try again.")
+                break
+            else:
+                print("Invalid answer. Please type 'M' or 'A'")
+    else:
+        for _ in range(5):
             warzone = automatic_ship(warzone)
+    print("\n All ships are placed. Here is your board:")
+    boards(warzone, board())
+    input("Please press 'ENTER'to start the game.")
     return warzone
 
 
